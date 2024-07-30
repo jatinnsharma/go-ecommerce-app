1e399a3f700dd5ac2de0101ca10c2b13aee47021
appdev-cts <appdev.cts@gmail.com>
2024-07-30 10:37:17 +0530

    go syntax simplified

---
+++ helper.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func HelperFunction(){
+	fmt.Println("This is helper function")
+}
--- main.go
+++ main.go
@@ -2,23 +2,63 @@ package main
 
 import (
 	"fmt"
-	"log"
+	_ "go-ecommerce-app/config" // underscore(_) used for not in use still package needed indirectly
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("I am main function")
-
 	// Create a new Fiber instance
+	// declare a variable and initialize
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// config.LoadAppSettings()
+	HelperFunction()
 
-	// Start the server on localhost:9000
-	if err := app.Listen("localhost:9000"); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+	// Basic Types: int , float64 , string ,bool
+	// Composite Types : array,slice,map,struct
+	// Pointer types: *
+
+	// var age int
+	// var height float64
+	// var firstName string
+	// var isEmployed bool
+
+	// age = 29
+	// height = 144.4
+	// firstName = "Jatin"
+	// isEmployed = true
+	// fmt.Println(age,height,firstName,isEmployed)
+
+	// fmt.Printf("Age: %v\n",age)
+	// fmt.Printf("Height: %v\n",height)
+	// fmt.Printf("FirstName: %v\n",firstName)
+	// fmt.Printf("Employed?:: %v\n",isEmployed)
+
+	// correct format specifier
+	// %d -> int , %f -> float , %s -> string , %t -> boolean
+
+	// automatically assign data (Type of data)
+	// At the time of declare go is going to be assigned
+	age := 29
+	height := 144.4
+	firstName := "Jatin"
+	isEmployed := true
+
+	fmt.Printf("Age: %d\n", age)
+	fmt.Printf("Height: %f\n", height)
+	fmt.Printf("FirstName: %s\n", firstName)
+	fmt.Printf("Employed?:: %t\n", isEmployed)
+
+	if age > 65 {
+		fmt.Println("Senior citizen")
+	} else if age > 16 {
+		fmt.Println("Adult")
 	}
+
+	for i := 0; i <= 10; i++ {
+		fmt.Println("Hello World!")
+	}
+
+	app.Listen("localhost:9000")
 }
